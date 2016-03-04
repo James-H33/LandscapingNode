fab8fd3619202a773c53a56ddf779f53d2b5b069
James Hall-Treworgy <James Hall-Treworgy>
2016-03-03 21:05:09 -0500

    Heroku Ready

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log('Server has started..')
+});
