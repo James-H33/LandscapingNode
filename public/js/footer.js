95025622d4aa65f99ccf0692b9524a6f4f813203
James Hall-Treworgy <James Hall-Treworgy>
2016-03-19 13:47:29 -0400

    Added Styling

--- public/js/footer.js
+++ public/js/footer.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	//Check to see if the window is top if not then display button
 	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
+		if ($(this).scrollTop() > 250) {
 			$('.scrollToTop').fadeIn();
 		} else {
 			$('.scrollToTop').fadeOut();
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
 	//Click event to scroll to top
 	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
+		$('html, body').animate({scrollTop : 0},500);
 		return false;
 	});
 
