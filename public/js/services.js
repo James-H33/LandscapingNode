e1a6f0a5c6bc8995639bae432792368e941b1d13
James Hall-Treworgy <James Hall-Treworgy>
2016-03-25 23:09:22 -0400

    Fixes for mobile and desktop in refernce to background image sizes

--- public/js/services.js
+++ public/js/services.js
@@ -2,10 +2,10 @@ $(window).scroll(function() {
     var scrollCheck = $(this).scrollTop();
 
     $('.introDisplay').css({
-      'transform' : 'translate(0,'+ scrollCheck / 30 +'%)'
+      'transform' : 'translate(0,'+ scrollCheck / 25 +'%)'
     })
 
-    if(scrollCheck > $('.midContainer').offset().top - ($(window).height() * .5)) {
+    if(scrollCheck > $('.midContainer').offset().top - ($(window).height() * .8)) {
 
       $('.midOneDisplay:nth-child(2)').css({
         'opacity' : '1',
@@ -13,7 +13,7 @@ $(window).scroll(function() {
       });
     }
 
-    if(scrollCheck > $('.midTwoDisplay').offset().top - ($(window).height() * .5)) {
+    if(scrollCheck > $('.midTwoDisplay').offset().top - ($(window).height() * .8)) {
       $('.midTwoDisplay').css({
         'opacity' : '1',
         'transform' : 'translateY(0px)'
